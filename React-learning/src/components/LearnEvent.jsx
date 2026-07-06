82980af8c675ba2a9cff7b2b123e97ed3bb491e3
MushibKhan <ahmedmusheeb1@gmail.com>
2026-07-06 15:01:29 +0530

    Event in JSX with or without Attribute

--- React-learning/src/components/LearnEvent.jsx
+++ React-learning/src/components/LearnEvent.jsx
@@ -5,10 +5,19 @@ const handleClick = (e) =>{
   console.log("Button clicked ")
 }
 
+const handleClick2 = (myId) =>{
+
+  console.log("Button clicked 2",myId)
+}
+
 export const LearnEvent = () => {
   return (
     <>
+    {/*Event in JSX without Attribute */}
     <button onClick={handleClick}>Click Lean event</button>
+
+    {/*Event in JSX with Attribute */}
+    <button onClick={(e)=>handleClick2("myID")}>Click 2</button>
     </>
   )
 }
